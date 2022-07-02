e9b7b6740f74e66b176520a034008e4baeb653c7
Azaz <azazwinner786@gmail.com>
2022-07-02 14:18:23 +0530

    day3 complited

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,8 +8,47 @@ class LoginPage extends StatelessWidget {
       child: Column(
         children: [
           Image.asset("images/login_image.png",
-        fit: BoxFit.cover,
-        ),],
+        fit: BoxFit.cover,  
+        ),
+        
+       SizedBox(
+        height: 20.0,
+       ),
+
+      Text("welcome",
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),),
+        
+         SizedBox(
+        height: 20.0,
+       ),
+
+       Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 32.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter username",
+                          labelText: "Username",
+                        ),   
+                      ),
+
+                       TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Enter password",
+                          labelText: "Password",
+                        ),),
+
+
+                    ]
+                  ),
+       ),
+                  ],
         )
     );
   }
